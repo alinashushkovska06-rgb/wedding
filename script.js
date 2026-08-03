a903524676d6ab56e20221ce5e8217f804508330
alinashushkovska06-rgb <alinashushkovska.06@gmail.com>
2026-08-03 12:03:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const envelope = document.getElementById("envelope");
+const flap = document.querySelector(".flap");
+const letter = document.querySelector(".letter");
 
+let opened = false;
+
+envelope.addEventListener("click", () => {
+
+    if (opened) return;
+
+    opened = true;
+
+    flap.style.transform = "rotateX(180deg)";
+    flap.style.zIndex = "0";
+
+    setTimeout(() => {
+        letter.style.transform = "translate(-50%, -45%)";
+    }, 500);
+
+});
