09da0c45bd8b1b3920caa15095af785e39459773
alinashushkovska06-rgb <alinashushkovska.06@gmail.com>
2026-08-07 15:01:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-// ==========================
-// WEDDING INVITATION 3.0
-// ==========================
-
-const envelope = document.querySelector(".envelope");
-const seal = document.getElementById("seal");
-const music = document.getElementById("music");
 const intro = document.getElementById("intro");
 const site = document.getElementById("site");
+const music = document.getElementById("music");
+const seal = document.getElementById("sealButton");
 
 let opened = false;
 
 seal.addEventListener("click", () => {
 
-    if(opened) return;
+    if (opened) return;
 
     opened = true;
 
-    // запускаємо музику
-    music.volume = 1;
+    music.play();
 
-    music.play().catch(() => {});
+    intro.style.transform = "scale(1.03)";
+    intro.style.opacity = "0";
 
-    // відкриваємо конверт
-    envelope.classList.add("open");
-
-    // показуємо сайт
     setTimeout(() => {
+
+        intro.style.display = "none";
 
         site.classList.add("show");
 
-    },1200);
-
-    // прибираємо заставку
-
-    setTimeout(() => {
-
-        intro.style.pointerEvents="none";
-
-        intro.style.opacity="0";
-
-        intro.style.transition="opacity 1s ease";
-
-    },1700);
+    }, 900);
 
 });
