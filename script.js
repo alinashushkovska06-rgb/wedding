f1fd5e7ab98eb96c6ccf7a6296443fdf01ce701c
alinashushkovska06-rgb <alinashushkovska.06@gmail.com>
2026-08-10 11:39:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,22 @@ const observer = new IntersectionObserver(
 animatedSections.forEach((section) => {
     observer.observe(section);
 });
+
+const revealElements = document.querySelectorAll(".reveal");
+
+const revealObserver = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+revealElements.forEach((element) => {
+    revealObserver.observe(element);
+});
